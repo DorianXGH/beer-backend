286896caa94ed055c0bf1472f01a0e84ca52680e
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-08-19 14:01:08 +0200

    added constructor

--- src/stellarium_control_protocol.rs
+++ src/stellarium_control_protocol.rs
@@ -25,12 +25,28 @@ struct Connection {
 };
 
 impl Connection {
-    fn readmsg($self) -> GotoMsg {
+    fn from_stream(&mut net::TcpStream stream) -> Connection {
+        Connection {
+            buffer : [0;std::mem::size_of(GotoMsg)],
+            stream : stream
+        }
+    }
+
+    fn readmsg($self) -> Option<GotoMsg> {
         let numawait = self.stream.peek(&mut self.buffer).unwrap();
         if(numawait == std::mem::size_of(GotoMsg))
         {
             self.stream.read(&mut self.buffer).unwrap();
-            
+            let goto = GotoMsg {
+                length : self.buffer[0] << 8 | self.buffer[1],
+                type : self.buffer[2] << 8 | self.buffer[3],
+                time : self.buffer[4] << 56 | self.buffer[5] << 48 | self.buffer[6] << 40 | self.buffer[7] << 32 | self.buffer[8] << 24 | self.buffer[9] << 16 | self.buffer[10] << 8 | self.buffer[11],
+                right_ascension : self.buffer[12] << 24 | self.buffer[13] << 16 | self.buffer[14] << 8 | self.buffer[15],
+                declisation : self.buffer[16] << 24 | self.buffer[17] << 16 | self.buffer[18] << 8 | self.buffer[19]
+            };
+            Some(goto)
+        } else {
+            None
         }
 
     }
