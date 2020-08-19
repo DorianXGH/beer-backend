6b08deef755520d194f10b25771a17f82e32cd9e
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-08-19 14:05:29 +0200

    testing something

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use stellarium_control_protocol::Connection;
 
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let con = Connection::from_stream(&mut stream)
+    con.readmsg().unwrap();
 }
