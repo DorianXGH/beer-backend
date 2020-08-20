9812aee528027f15a20184ac808714166a146f36
DorianXGH <dorian.bourgeoisat@icloud.com>
2020-08-20 16:15:50 +0200

    fixed bug : it was endianness

--- src/stellarium_control_protocol.rs
+++ src/stellarium_control_protocol.rs
@@ -40,11 +40,11 @@ impl<'a> Connection <'a> {
         {
             self.stream.read(&mut self.buffer).unwrap();
             let goto = GotoMsg {
-                length : (self.buffer[0] as u16) << 8 | (self.buffer[1] as u16),
-                msg_type : (self.buffer[2] as u16) << 8 | (self.buffer[3] as u16),
-                time : (self.buffer[4] as u64) << 56 | (self.buffer[5] as u64) << 48 | (self.buffer[6] as u64) << 40 | (self.buffer[7] as u64) << 32 | (self.buffer[8] as u64) << 24 | (self.buffer[9] as u64) << 16 | (self.buffer[10] as u64) << 8 | (self.buffer[11] as u64),
-                right_ascension : (self.buffer[12] as u32) << 24 | (self.buffer[13] as u32) << 16 | (self.buffer[14] as u32) << 8 | (self.buffer[15] as u32),
-                declination : ((self.buffer[16] as u32) << 24 | (self.buffer[17] as u32) << 16 | (self.buffer[18] as u32) << 8 | (self.buffer[19] as u32)) as i32
+                length : (self.buffer[1] as u16) << 8 | (self.buffer[0] as u16),
+                msg_type : (self.buffer[3] as u16) << 8 | (self.buffer[2] as u16),
+                time : (self.buffer[11] as u64) << 56 | (self.buffer[10] as u64) << 48 | (self.buffer[9] as u64) << 40 | (self.buffer[8] as u64) << 32 | (self.buffer[7] as u64) << 24 | (self.buffer[6] as u64) << 16 | (self.buffer[5] as u64) << 8 | (self.buffer[4] as u64),
+                right_ascension : (self.buffer[15] as u32) << 24 | (self.buffer[14] as u32) << 16 | (self.buffer[13] as u32) << 8 | (self.buffer[12] as u32),
+                declination : ((self.buffer[19] as u32) << 24 | (self.buffer[18] as u32) << 16 | (self.buffer[17] as u32) << 8 | (self.buffer[16] as u32)) as i32
             };
             Some(goto)
         } else {
